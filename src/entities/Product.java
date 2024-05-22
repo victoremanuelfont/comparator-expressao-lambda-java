9fa439e095b47b46f02111cad5d3785054da3990
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-05-22 11:40:06 -0300

    Segunda implementação

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,6 +1,6 @@
 package entities;
 
-public class Product implements Comparable<Product> {
+public class Product {
 
 	private String name;
 	private Double price;
@@ -34,8 +34,5 @@ public class Product implements Comparable<Product> {
 		return "Product [name=" + name + ", price=" + price + "]";
 	}
 
-	@Override
-	public int compareTo(Product p) {
-		return name.toUpperCase().compareTo(p.getName().toUpperCase());
-	}
+
 }
